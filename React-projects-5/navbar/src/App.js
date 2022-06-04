ea77d65bd8264afc095a97fa836f89d3d18ab0bf
Hayley Wood <onlinecowork@gmail.com>
2022-06-04 20:42:06 +1000

    auto height, desktop/mobile

--- React-projects-5/navbar/src/App.js
+++ React-projects-5/navbar/src/App.js
@@ -1,3 +1,52 @@
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { links, socialIcons } from "./data";
+import React from "react";
+
 export default function App() {
-  return <></>;
+  const [showLinks, setShowLinks] = React.useState(false);
+  const linksRef = React.useRef(null);
+  const linksContainerRef = React.useRef(null);
+
+  React.useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = `0px`;
+    }
+  }, [showLinks]);
+
+  return (
+    <nav className='flex justify-between'>
+      <div className='nav-header flex justify-between'>
+        <img src={logo} alt='logo'></img>
+        <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+          <FaBars />
+        </button>
+      </div>
+      <div className='links-container show-container' ref={linksContainerRef}>
+        <ul className='links flex' ref={linksRef}>
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <ul className='social-icons flex'>
+        {socialIcons.map((singleIcon) => {
+          const { id, url, icon } = singleIcon;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
+  );
 }
---
+++ React-projects-5/navbar/src/data.js
@@ -0,0 +1,47 @@
+import { FaFacebook, FaInstagram, FaReddit, FaTwitter } from "react-icons/fa";
+
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/contact",
+    text: "contact",
+  },
+  {
+    id: 4,
+    url: "/products",
+    text: "products",
+  },
+];
+
+export const socialIcons = [
+  {
+    id: 1,
+    url: "https://altcampus.com",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://altcampus.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://altcampus.com",
+    icon: <FaInstagram />,
+  },
+  {
+    id: 4,
+    url: "https://altcampus.com",
+    icon: <FaReddit />,
+  },
+];
